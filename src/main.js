93daacab09c83dbc0e6cd32cc5b7159725f242ee
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-01-26 16:51:40 +0800

    升級node16

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import i18n from "./i18n";
 import "@/assets/css/reset.css";
+import "element-plus/theme-chalk/display.css";
 import "@/assets/css/element.css";
 import "animate.css";
 
