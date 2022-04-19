c52d4bc0719f312137511f5b3983351a674f0981
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-04-19 09:16:14 +0800

    修正vue router bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <nav>
-    <router-link :to="{ name: 'TestView' }">TestView</router-link> |
-    <router-link :to="{ name: 'goldsummary' }"
+    <router-link
+      :to="{ name: 'TestView' }"
+      :class="{ active: nRouteIndex === 0 }"
+      >TestView</router-link
+    >
+    |
+    <router-link
+      :to="{ name: 'goldsummary' }"
+      :class="{ active: nRouteIndex === 1 }"
       >黃金存摺帳戶內容查詢</router-link
     >
   </nav>
@@ -12,6 +19,28 @@
 export default {
   name: "App",
 };
+</script>
+
+<script setup>
+import { useRoute } from "vue-router";
+import { ref, watch } from "vue";
+
+let aRouterList = ["", "golden"];
+
+const route = useRoute();
+const nRouteIndex = ref(0);
+watch(
+  () => route.path,
+  () => {
+    const sNowMainPath = route.path.split("/")[1];
+
+    aRouterList.forEach((item, index) => {
+      if (item === sNowMainPath) {
+        nRouteIndex.value = index;
+      }
+    });
+  }
+);
 </script>
 
 <style lang="scss">
@@ -30,7 +59,7 @@ nav {
     font-weight: bold;
     color: #2c3e50;
 
-    &.router-link-exact-active {
+    &.active {
       color: #42b983;
     }
   }
